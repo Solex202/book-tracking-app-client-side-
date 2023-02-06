98da429b9eccec19bb25b22ba8c62492036b5609
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-02-06 02:20:52 +0100

    making use of react router

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,39 +1,54 @@
-import React from 'react'
+import React, { useState} from 'react'
 import { useForm } from "react-hook-form";
+import { Link } from 'react-router-dom';
 
 
 const Register = () => {
-  
   const { register, handleSubmit, errors } = useForm();
-  const onSubmit = (data) => console.log(data);
+  // const onSubmit = (data) => console.log(data);
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmits = async (event) => {
+    event.preventDefault();
+    const response = await fetch('http://localhost:8080/users', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json', 
+      'Access-Control-Allow-Origin' : 'http://localhost:3000/'
+     },
+      body: JSON.stringify({ username, password }),
+    });
+    const data = await response.json();
+    console.log(data);
+    // setMessage(data.message);
+  };
+
 
   return (
 
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={(e)=>handleSubmits(e)}>
       <input
         type="text"
-        name="username"
-        ref={register({ required: true })}
+        placeholder="username"
+        
+        onChange={(e)=> setUsername(e.target.value)}
+        // ref={register({ required: true })}
       />
-      {errors.name && <p>This field is required</p>}
+      {/* {errors.name && <p>This field is required</p>} */}
 
       <input
         type="passowrd"
-        name="password"
-        ref={register({ required: true })}
+        placeholder="passwords"
+        onChange={(e)=> setPassword(e.target.value)}
+        // ref={register({ required: true })}
       />
-      {errors.name && <p>This field is required</p>}
+      {/* {errors.name && <p>This field is required</p>} */}
 
-      <input type="submit" />
+      <Link to="/welcome"><input type="submit" /></Link>
     </form>
 
-    // <div>
-    //     <form>
-    //         <input type="text" placeholder="username"/> 
-    //         <input type="password" placeholder="password"/> 
-    //         <button>submit</button>
-    //     </form>
-    // </div>
+  
   )
 }
 
